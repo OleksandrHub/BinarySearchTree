a24b22cb2755660c7b5b40b95862d37b5a7e94c8
OleksandrHub <sasha.klishch1@gmail.com>
2025-02-10 20:20:39 +0200

    feat(bst): add search and delete methods
    - Implemented search functionality
    - Added delete method for nodes
    - Improved TreeNode __str__ method
    - Added basic test cases in main
    - Enhanced code readability

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class TreeNode:
         self.key = key
         self.left = None
         self.right = None
+    def __str__(self):
+        return str(self.key)
 
 class BinarySearchTree:
     def __init__(self):
@@ -16,4 +18,22 @@ class BinarySearchTree:
             node.right = self._insert(node.right, key)
         return node
     def insert(self, key):
-        pass
+        self.root = self._insert(self.root, key)
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+    def search(self, key):
+        return self._search(self.root, key)
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+    
+bst = BinarySearchTree()
+nodes = [50, 30, 20, 40, 70, 60, 80]
+(bst.insert(node) for node in nodes)
+#print('Search for 80:', bst.search(80))
